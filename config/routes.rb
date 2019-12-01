6ca1e2d0d7c12f9fb9d999e17b6480e2b065a95f
Jamie Berrier <berrier.jamie@gmail.com>
2019-12-01 18:54:11 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :artists, only: [:show] do
+  resources :artists do
     # nested resource for songs
     resources :songs, only: [:show, :index]
   end
