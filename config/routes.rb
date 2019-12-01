3bc28c2f3ae4fb31b33102f9aa498174418c4bd2
Jamie Berrier <berrier.jamie@gmail.com>
2019-12-01 18:15:26 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :artists
   resources :songs
 
-  get 'authors/:id/posts', to: 'authors#posts_index'
+  get 'artists/:id/songs', to: 'artists#songs_index'
   get 'authors/:id/posts/:post_id', to: 'authors#post'
 end
