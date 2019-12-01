7e497c4915c6570310944e0400e22dcdf6dbca4f
Jamie Berrier <berrier.jamie@gmail.com>
2019-12-01 18:51:10 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,6 @@
 class SongsController < ApplicationController
   def index
-    binding.pry
+    #binding.pry
     if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
       if @artist
