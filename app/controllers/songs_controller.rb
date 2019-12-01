1a33a6f05043a34534f4752262e0c0017e0b9b0e
Jamie Berrier <berrier.jamie@gmail.com>
2019-12-01 18:43:01 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,9 @@
 class SongsController < ApplicationController
   def index
     if params[:artist_id]
-      @songs = Artist.find(params[:artist_id]).songs
+      @artist = Artist.find(params[:artist_id])
+      if @artist
+      #@songs = Artist.find(params[:artist_id]).songs
     else
       @songs = Song.all
     end
