7d2f67eeae6970bbaa691507611381b1777c7936
Jamie Berrier <berrier.jamie@gmail.com>
2019-12-01 18:29:11 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   resources :songs, only: [:index, :show, :new, :create, :edit, :update]
 
+  root 'songs#index'
   #get 'artists/:id/songs', to: 'artists#songs_index', only: [:index, :show]
   #get 'artists/:id/songs/:song_id', to: 'artists#song', only: [:index, :show]
 end
