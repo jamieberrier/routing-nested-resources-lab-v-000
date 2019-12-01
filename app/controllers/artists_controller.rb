7453fe0b04861add0be119121be9832490167e91
Jamie Berrier <berrier.jamie@gmail.com>
2019-12-01 18:20:33 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -10,6 +10,7 @@ class ArtistsController < ApplicationController
   def songs_index
     @artist = Artist.find(params[:id])
     @songs = @artist.songs
+    render template: 'songs/index'
   end
 
   def song
