e133ccee559b80317922bd42d7a9db5907e0b85a
Jamie Berrier <berrier.jamie@gmail.com>
2019-12-01 18:44:43 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,6 +6,7 @@ class SongsController < ApplicationController
         @songs = @artist.songs
       else
         flash[:alert] = "Artist not found"
+        redirect_to artists_path
       end
     else
       @songs = Song.all
